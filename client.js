c06f4f33bf1e43e9bbecfb3455e22d64284a64cb
Ian Johnson <enjalot@gmail.com>
2015-02-21 14:03:48 -0800

    hack together racer 0.6 version

--- client.js
+++ client.js
@@ -1,16 +1,34 @@
-var racer = require("racer");
-require("racer-browserchannel/lib/browser");
-
-racer.load = function(url, cb) {
-  var xhr = new XMLHttpRequest();
-  racer._model = null;
-
-  xhr.onload = function() {
-    racer.ready(cb);
-    racer.init(JSON.parse(this.responseText));
+// AMD compatible
+define([], function(){
+  var racer = require("racer");
+  
+  // This isn't browserifiable directly
+  //require("racer-browserchannel/lib/browser");
+  /////// copied from lib/browser
+  //var racer = require('racer');
+  var BCSocket = require('./node_modules/racer-browserchannel/node_modules/browserchannel/dist/bcsocket-uncompressed').BCSocket;
+  // TODO: this is normally done in the browserify
+  var CLIENT_OPTIONS = {reconnect: true, base: "/racer/channel"}
+  racer.Model.prototype._createSocket = function() {
+    var base = CLIENT_OPTIONS.base || '/channel';
+    return new BCSocket(base, CLIENT_OPTIONS);
   };
-  xhr.open("get", url, true);
-  xhr.send();
-};
+  ////////////
 
-module.exports = racer;
+  racer.load = function(url, cb) {
+    var xhr = new XMLHttpRequest();
+    racer._model = null;
+
+    xhr.onload = function() {
+      var model = racer.createModel(JSON.parse(this.responseText))
+      cb(null, model);
+    };
+    xhr.open("get", url, true);
+    xhr.send();
+  };
+
+  console.log("middleware racer", racer)
+  //module.exports = racer;
+  return racer
+
+})
--- index.js
+++ index.js
@@ -4,13 +4,13 @@ module.exports = function(opts) {
   var store = require("racer").createStore({
     db: opts.db
   }),
-  b = require("racer/node_modules/browserify")(path.join(__dirname, "client.js")),
+  b = require("browserify")(path.join(__dirname, "client.js")),
   modelMiddleware = store.modelMiddleware(store),
   racerBrowserChannel = require("racer-browserchannel")(store),
   storeBundle;
 
   b.require(path.join(__dirname, "node_modules", "node-uuid")); //We need to replace racer's uuid because the version that it uses breaks when required via AMD.
-  b.bundle({standalone: "racer"}, function(err, bundle) {
+  b.bundle(function(err, bundle) {
     if (err) throw err;
     storeBundle = bundle;
   });
@@ -28,7 +28,6 @@ module.exports = function(opts) {
   }
 
   return function(req, res, next) {
-    var path = this.route;
 
     if (req.url === "/") {
       res.setHeader("Content-Type", "text/javascript");
@@ -39,11 +38,15 @@ module.exports = function(opts) {
 
     modelMiddleware(req, res, function() {
       racerBrowserChannel(req, res, function() {
-        var model = req.getModel(),
-        route = opts.routes && opts.routes[req.url.substr(1)],
-        done = function() {
+        var model = req.getModel();
+        // TODO: proper route matching. this is just a shortcut, not full REST
+        var base = req.url.substr(1)//.split('/')[0];
+        var route = opts.routes && opts.routes[base];
+        //console.log("ROUTE?", route, opts.routes, req.url.substr(1));
+        var done = function() {
           model.bundle(function(err, modelBundle) {
-            modelBundle.racerBrowserChannel.base = path + "/channel";
+            // TODO this is supposed to be the same in client.js
+            modelBundle.racerBrowserChannel = {base: "/racer/channel" };
             res.setHeader("Content-Type", "application/json");
             res.end(JSON.stringify(modelBundle));
           });
